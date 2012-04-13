14f542fe3b513323873ca24078608312346098c4
Steve Faulkner <southpolesteve@gmail.com>
2012-04-13 00:04:42 -0500

    Add initial code to set session uuid

--- lib/railitics/engine.rb
+++ lib/railitics/engine.rb
@@ -1,9 +1,26 @@
+require "uuidtools"
+
 module Railitics
   class Engine < ::Rails::Engine
     isolate_namespace Railitics
 
+    initializer "testing" do |app|
+      ActionController::Base.send :include, Railitics::InstanceMethods
+    end
+
     config.generators do |g|
       g.test_framework :rspec, :view_specs => false
+    end
+  end
+end
+
+
+module Railitics
+  module InstanceMethods
+    def track_user
+      unless session[:railitics]
+        session[:railitics] = UUIDTools::UUID.timestamp_create
+      end
     end
   end
 end
