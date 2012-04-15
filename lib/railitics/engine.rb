098cc0f603f10a8f6f0d828c94f3981d26179f87
Steve Faulkner <southpolesteve@gmail.com>
2012-04-15 17:32:04 -0500

    Small refactor of simple tracking code

--- lib/railitics/engine.rb
+++ lib/railitics/engine.rb
@@ -1,4 +1,5 @@
 require "uuidtools"
+require "pry"
 
 module Railitics
   class Engine < ::Rails::Engine
@@ -18,9 +19,36 @@ end
 module Railitics
   module InstanceMethods
     def track_user
-      unless session[:railitics]
+      set_cookie
+      if session[:railitics] || !current_user
+        log_request uuid: session[:railitics]
+      end
+      if current_user && session[:railitics]
+        log_request user_id: current_user.id
+        if session[:railitics]
+          update_all_session_requests
+        end
+      end
+    end
+
+    private
+
+    def set_cookie
+      unless session[:railitics] || current_user
         session[:railitics] = UUIDTools::UUID.timestamp_create
       end
     end
+
+    def log_request(opts = {})
+      opts.merge params: params, method: request.method
+      Railitics::Request.create(opts)
+    end
+
+    def update_all_session_requests
+      Railitics::Request.where(uuid: session[:railitics]).update_all(user_id: current_user.id)
+      session[:railitics] = nil
+    end
+
   end
 end
+
