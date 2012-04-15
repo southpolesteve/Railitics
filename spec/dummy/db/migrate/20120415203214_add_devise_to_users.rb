b93ff23f4fd7af9833da79cb5b377b8b93921ba2
Steve Faulkner <southpolesteve@gmail.com>
2012-04-15 17:32:31 -0500

    Devise install

---
+++ spec/dummy/db/migrate/20120415203214_add_devise_to_users.rb
@@ -0,0 +1,32 @@
+class AddDeviseToUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users
+
+    change_table(:users) do |t|
+      ## Database authenticatable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
+
+
+      ## Trackable
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      ## Encryptable
+      t.string :password_salt
+
+      t.timestamps
+    end
+
+    add_index :users, :email,                :unique => true
+  end
+
+  def self.down
+    # By default, we don't want to make any assumption about how to roll back a migration when your
+    # model already existed. Please edit below which fields you would like to remove in this migration.
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
