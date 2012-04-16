e9c0e114cd61cb67138053d63b5c9826f7ef41bb
Steve Faulkner <southpolesteve@gmail.com>
2012-04-15 22:34:32 -0400

    Switch to index instead of home

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root :to => "content#home"
+  root :to => "content#index"
   post "/post" => "content#post"
+  put "/put" => "content#put"
   delete "/delete" => "content#delete"
 
   mount Railitics::Engine => "/railitics"
