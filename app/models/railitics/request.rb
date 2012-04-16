84a9ca1ad8c55026d37eaa82ab37efb146f0b35f
Steve Faulkner <southpolesteve@gmail.com>
2012-04-15 22:01:34 -0400

    Define methods for action and controller

--- app/models/railitics/request.rb
+++ app/models/railitics/request.rb
@@ -4,6 +4,17 @@ module Railitics
   class Request
     include Mongoid::Document
     field :uuid, type: String
-    field :user_id
+    field :user_id, type: Integer
+    field :params, type: Hash
+    field :method, type: String
+
+    def controller
+      params["controller"]
+    end
+
+    def action
+      params["action"]
+    end
+
   end
 end
