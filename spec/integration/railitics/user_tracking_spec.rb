3a866dbc1f599e56d700c04b33daf6c08a872197
Steve Faulkner <southpolesteve@gmail.com>
2012-04-15 22:52:53 -0400

    Move test code, alter description

--- spec/integration/railitics/user_tracking_spec.rb
+++ spec/integration/railitics/user_tracking_spec.rb
@@ -8,19 +8,9 @@ describe "user_tracking" do
     Railitics::Request.delete_all
   end
 
-  it "creates one request record based on UUID" do
+  it "creates one request record based on UUID with one get request" do
     visit root_path
     Railitics::Request.count.should eq(1)
-  end
-
-  it "creates request records and associates with user ID" do
-    user = User.create!(email: "user@example.com", password: 'password', password_confirmation: 'password')
-    visit new_user_session_path
-    fill_in "Email", with: "user@example.com"
-    fill_in "Password", with: "password"
-    click_button "Sign in"
-    Railitics::Request.all.map(&:user_id).uniq.size.should eq(1)
-    Railitics::Request.all.map(&:user_id).uniq.first.should eq(user.id)
   end
 
   it "successfully logs a POST request" do
@@ -39,6 +29,20 @@ describe "user_tracking" do
     visit root_path
     click_link "DELETE"
     Railitics::Request.where(method: "DELETE").count.should eq(1)
+  end
+
+  it "creates request records and associates with user ID" do
+    visit root_path
+    click_link "POST"
+    click_link "PUT"
+    click_link "DELETE"
+    user = User.create!(email: "user@example.com", password: 'password', password_confirmation: 'password')
+    visit new_user_session_path
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+    Railitics::Request.all.map(&:user_id).uniq.size.should eq(1)
+    Railitics::Request.all.map(&:user_id).uniq.first.should eq(user.id)
   end
 
 
