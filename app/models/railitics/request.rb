63c3e7168fa90c826a016ea670b656583b6cfd1b
Steve Faulkner <southpolesteve@gmail.com>
2012-04-16 22:18:12 -0400

    Add default scope and timestamps to model

--- app/models/railitics/request.rb
+++ app/models/railitics/request.rb
@@ -3,11 +3,14 @@ require 'mongoid'
 module Railitics
   class Request
     include Mongoid::Document
+    include Mongoid::Timestamps::Created
     field :uuid, type: String
     field :user_id, type: Integer
     field :params, type: Hash
     field :method, type: String
     field :referrer, type: String
+
+    default_scope order_by([:created_at, :desc])
 
     def controller
       params["controller"]
