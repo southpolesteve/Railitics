ddfef62d0f8b4611f2269a67dae32bddcf47cc49
Steve Faulkner <southpolesteve@gmail.com>
2012-04-18 16:55:15 -0400

    Scope request based on params

--- app/controllers/railitics/requests_controller.rb
+++ app/controllers/railitics/requests_controller.rb
@@ -1,7 +1,13 @@
 module Railitics
   class RequestsController < ApplicationController
+    layout "railitics/application"
+
     def index
-      @requests = Railitics::Request.all
+      query = Railitics::Request.scoped
+      query = query.where(user_id: params[:user_id]) if params[:user_id]
+      query = query.where(uuid: params[:uuid]) if params[:uuid]
+      @requests = query.all
     end
+    
   end
 end
